bef3c0eea48344428fe129148072fa72178c7135
payal-04 <payal078601payal@gmail.com>
2025-06-27 21:21:19 +0530

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route,Routes, useLocation } from 'react-router-dom'
+import Home from './Pages/Home';
+import Footer from './components/Footer';
+import AllRooms from './Pages/AllRooms';
+import RoomDetails from './Pages/RoomDetails';
+import MyBookings from './Pages/MyBookings';
+import HotelReg from './components/HotelReg';
+
+const App = () =>
+{
+   const isOwnerPath = useLocation().pathname.includes("owner");
+
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar/>}
+      {false && <HotelReg />}
+      <div className='min-h-[70vh]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/rooms' element={<AllRooms/>}/>
+        <Route path='/rooms/:id' element={<RoomDetails/>} />
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
