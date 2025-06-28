a33e0a90a616af5cb1da9136541550addb7cbcd6
payal-04 <payal078601payal@gmail.com>
2025-06-28 23:19:29 +0530

    commit

--- client/src/components/hotelOwner/Navbar.jsx
+++ client/src/components/hotelOwner/Navbar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-   
+    <div className='flex items-center justify-between px-4 md:px-8 border-b
+    border-gray-300 py-3 bg-white transition-all duration-300'>
+    
     </div>
   )
 }
