3478d50183738273bff6bbc1ca4927d1c53a5eed
payal-04 <payal078601payal@gmail.com>
2025-06-30 00:53:13 +0530

    commit

--- client/src/Pages/hotelOwner/AddRoom.jsx
+++ client/src/Pages/hotelOwner/AddRoom.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../../components/Title'
 
 const AddRoom = () => {
+
+  const [images,setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null
+  })
+  const [inputs, setInputs] = useState({
+    roomType: '',
+    pricePerNight: 0,
+    amenities: {
+      'Free WiFi' : false,
+      'Free Breakfast' : false,
+      'Room Service' : false,
+      'Mountain View' : false,
+      'Pool Access' : false
+    }
+  })
+
   return (
-    <div>
-      
-    </div>
+    <form>
+      <Title align='left' font='Outfit' title='Add Room'
+      subTitle='Fill in the details carefully and accurate room details, pricing and amenities, to 
+      enhance the user booking experience.'/>
+   </form>
   )
 }
 
