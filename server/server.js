8cbedb77cc6ce352b034b3d8e97b1499441ec7aa
payal-04 <payal078601payal@gmail.com>
2025-06-30 19:27:19 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import cors from "cors"
 const app = express()
 app.use(cors()) //Enable Cross Origin Resource Sharing//
 
-app.get('/',(req,res)=> res.send("API is working"))
+app.get('/',(req,res)=> res.send("API is working fine"))
 
 const PORT = process.env.PORT || 3000;
 
