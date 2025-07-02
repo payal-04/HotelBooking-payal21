2d8ae3924ff6b74c674cee4bb015aa276202585a
payal-04 <payal078601payal@gmail.com>
2025-07-02 20:15:53 +0530

    commit

--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -4,7 +4,21 @@ import User from "../models/User.js";
 export const registerHotel = async(req,res)=>{
      try {
         const {name,address, contact, city} = req.body
+        const owner = req.user._id
+
+        //Check if User is already registered//
+        const hotel = await Hotel.findOne({owner})
+        if(hotel){
+            return res.json({success: false, message: "Hotel is Already Registered"})
+        }
+
+        await Hotel.create({name, address, contact, city, owner});
+
+        await User.findByIdAndUpdate(owner, {role: "hotelOwner"})
+
+        res.json({success: true, message: "Hotel Registered Successfully"})
+
      } catch (error) {
-        
+        res.json({success: false, message: error.message})
      }
 }
