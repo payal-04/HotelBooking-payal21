c5b22f0bbc805c51b4e671ae9943fae5223c2d1e
payal-04 <payal078601payal@gmail.com>
2025-07-02 20:59:26 +0530

    commited

---
+++ server/controllers/roomController.js
@@ -0,0 +1,24 @@
+//API to create new room for a hotel//
+
+export const  createRoom = async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//API to get all Rooma//
+export const getRooms = async(req,res)=>{\
+
+} 
+
+//API to get all rooms for a specific Hotel//
+export const getOwnerRooms = async(req,res)=>{
+
+}
+
+//API to toggle availability of a room//
+export const toggleRoomAvailability = async(req,res)=>{
+
+}
