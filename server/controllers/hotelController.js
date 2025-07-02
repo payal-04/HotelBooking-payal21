400c95784db2498edb844004715e29dd7203aafe
payal-04 <payal078601payal@gmail.com>
2025-07-02 20:10:00 +0530

    ok

---
+++ server/controllers/hotelController.js
@@ -0,0 +1,10 @@
+import Hotel from "../models/Hotel.js";
+import User from "../models/User.js";
+
+export const registerHotel = async(req,res)=>{
+     try {
+        const {name,address, contact, city} = req.body
+     } catch (error) {
+        
+     }
+}
